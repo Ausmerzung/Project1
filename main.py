7ab1cd424a30f8b01af575280bb8f6ff98e64818
isach <isachenkoartem61@gmail.com>
2025-07-10 17:08:13 +0300

    add files

---
+++ main.py
@@ -0,0 +1,48 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import ChatJoinRequest,Message, user
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+bot =Bot(token=os.getenv('BOT_TOKEN'))
+dp = Dispatcher()
+pending_requests = {}
+
+async def decline(user_id:int, chat_id:int):
+    await asyncio.sleep(60)
+
+    if user_id in pending_requests:
+        await bot.decline_chat_join_request(chat_id=chat_id, user_id=user_id)
+        await bot.send_message(user_id, "время вышло. заявка отклонена")
+        del pending_requests[user_id]
+
+@dp.chat_join_request()
+async def join_request(join_request:ChatJoinRequest):
+    user_id = join_request.from_user.id
+    chat_id = join_request.chat.id
+
+    task = asyncio.create_task(decline(user_id,chat_id))
+    pending_requests[user_id] = task
+
+    await bot.send_message(user_id, "заявка обрабатывается")
+
+
+@dp.message()
+async def any_message(message:Message):
+    user_id = message.from_user.id
+
+    if user_id in pending_requests:
+        pending_requests[user_id].cancel()
+        del pending_requests[user_id]
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Shutting down')
